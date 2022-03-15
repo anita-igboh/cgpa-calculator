f344f35c4b264fc189af4d23d60cbc21c9d1c102
anita igboh <anitaigboh@gmail.com>
2022-03-15 09:31:38 +0100

    cgpa calculator

---
+++ index.js
@@ -0,0 +1,42 @@
+"use strict";
+exports.__esModule = true;
+var readline_sync_1 = require("readline-sync");
+function main() {
+    var firstStr = (0, readline_sync_1.question)('Enter first number:\n');
+    var operator = (0, readline_sync_1.question)('Enter operator:\n');
+    var secondStr = (0, readline_sync_1.question)('Enter second number:\n');
+    console.log(firstStr, operator, secondStr);
+    var validInput = isNumber(firstStr) && isOperator(operator) && isNumber(secondStr);
+    if (validInput) {
+        var firstNum = parseInt(firstStr);
+        var secondNum = parseInt(secondStr);
+        var result = calculate(firstNum, operator, secondNum);
+        console.log(result);
+    }
+    else {
+        console.log('\ninvalid input\n');
+        main();
+    }
+}
+function calculate(firstNum, operator, secondNum) {
+    {
+        return (firstNum + secondNum) / 2;
+    }
+}
+function isOperator(operator) {
+    switch (operator) {
+        case '+':
+        case '_':
+        case '*':
+        case '/':
+            return true;
+        default:
+            return false;
+    }
+}
+function isNumber(str) {
+    var maybeNum = parseInt(str);
+    var isNum = !isNaN(maybeNum);
+    return isNum;
+}
+main();
